fe3222b4e47fbc7e39d03aa6107ef41eb39b9037
Dariia Nikolaieva <74855191+DariiaNikolaieva@users.noreply.github.com>
2021-11-14 09:13:38 +0200

    transaction schema
    - добавлена схема транзакций;

---
+++ model/transaction_schema.js
@@ -0,0 +1,37 @@
+const { Schema, model, SchemaTypes } = require('mongoose');
+
+const transactionsSchema = new Schema(
+  {
+    type: {
+      type: String,
+    },
+    category: {
+      type: String,
+    },
+    amount: {
+      type: Number,
+    },
+    date: {
+      type: Number,
+    },
+    comment: {
+      type: String,
+    },
+    owner: {
+      type: SchemaTypes.ObjectId,
+      ref: 'user',
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: function (doc, ret) {
+        delete ret._id;
+        return ret;
+      },
+    },
+    toObject: { virtuals: true },
+  },
+);
