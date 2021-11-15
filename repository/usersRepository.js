3e91c89f412223f7d87860366ba155c59d018692
MishaDreamer-web <shondayn@gmail.com>
2021-11-15 02:23:14 +0200

    Add login , logout , auth(jwt) point and fix signup

--- repository/usersRepository.js
+++ repository/usersRepository.js
@@ -1,4 +1,8 @@
 const User = require('../model/user_schema');
+// add non duplicate user id
+const findById = async id => {
+  return await User.findById(id);
+};
 // add non duplicate user email
 const findByEmail = async email => {
   return await User.findOne({ email });
@@ -8,8 +12,14 @@ const create = async options => {
   const user = new User(options);
   return await user.save();
 };
+// add updateToken for user
+const updateToken = async (id, token) => {
+  return await User.updateOne({ _id: id }, { token });
+};
 
 module.exports = {
+  findById,
   findByEmail,
   create,
+  updateToken,
 };
