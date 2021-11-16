6064dcc2da74edb9df3facf4bf0ac4cccc39b64a
Dariia Nikolaieva <74855191+DariiaNikolaieva@users.noreply.github.com>
2021-11-16 18:41:09 +0200

    add enpoints delete and edit transactions
    - edit transaction schema;
    - edit endpoint get all transactions;
    - create endpoint edit transaction by id;
    - create endpoint delete transaction by id;

--- repository/transactionsRepository.js
+++ repository/transactionsRepository.js
@@ -1,14 +1,31 @@
-const Transactions = require('../model/transaction_schema');
+const Transaction = require('../model/transaction_schema');
 
 const getAllTransactions = async (userId) => {
-    return await Transactions.find({owner: userId})
+    return await Transaction.find({owner: userId})
 }
 
 const addTransaction = async (body) => {
-    return await Transactions.create(body);
+    return await Transaction.create(body);
 }
+
+const editTransaction = async (transactionId, body, userId) => {
+    return await Transaction.findOneAndUpdate(
+        { _id: transactionId, owner: userId },
+        { ...body },
+        { new: true }
+      );
+}
+
+const deleteTransaction = async (transactionId, userId) => {
+    return await Transaction.findOneAndRemove({
+    _id: transactionId,
+    owner: userId,
+  });
+};
 
 module.exports = {
     getAllTransactions,
     addTransaction,
+    editTransaction,
+    deleteTransaction,
 }
